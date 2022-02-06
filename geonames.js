5993e3fb6c19d5a2d4f78c559f05842cbc9193d7
borquezz <jesusfernandoborquez@gmail.com>
2022-02-05 17:38:13 -0700

    [Edit] retrieving earthquakes done

--- geonames.js
+++ geonames.js
@@ -1,12 +1,21 @@
-// import fetch from "node-fetch";
-const getEarthquakes = async () => {
-  let coords = [44.1, -9.9, -22.4, 55.2];
+import { GoogleMap } from "./map.js";
+
+async function getEarthquakes(bounds) {
   const result = await fetch(
-    `http://api.geonames.org/citiesJSON?north=${coords[0]}&south=${coords[1]}&east=${coords[2]}&west=${coords[3]}&lang=de&username=reinier`
+    `http://api.geonames.org/citiesJSON?north=${bounds.north}&south=${bounds.south}&east=${bounds.east}&west=${bounds.west}&lang=de&username=reinier`
   );
 
   const data = await result.json();
   return data;
-};
-const earthquakes = await getEarthquakes();
-localStorage.setItem("data", JSON.stringify(earthquakes.geonames));
+}
+
+let map = new GoogleMap();
+
+document.getElementById("submit").addEventListener("click", async function () {
+  let address = await map.geoCode();
+  map.updateAddress(address.results[0]);
+  const earthquakes = await getEarthquakes(map.bounds);
+  earthquakes.geonames.forEach((location) => {
+    map.addMarker({ lat: location.lat, lng: location.lng });
+  });
+});
